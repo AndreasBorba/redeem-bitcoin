d9aa6528b770e2bb9f5473a9fb0c6d34530d0d72
Andreas Borba <borba_andreas@hotmail.com>
2025-01-08 01:59:46 -0300

    Inserção de dados na tela

--- matriz.py
+++ matriz.py
@@ -4,8 +4,10 @@ co.headless = False
 
 browser = Chromium(co)
 tab = browser.latest_tab
+tab.set.window.max()
 tab.get('https://freebitco.in/')
-
+tab.ele('#signup_form_email').input('teste@gmail.com')
+tab.ele('#signup_form_password').input('123456')
 input('Press Enter to close the browser...')
 browser.quit()
 
